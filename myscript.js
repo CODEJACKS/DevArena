b9a266ee63b9bf3cc6380273fb81e70e94f94bec
CJhauser <1019628@rochesterschools.org>
2024-12-16 14:00:11 -0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,6 +3,26 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker registered successfully.');
   });
 }
+window.addEventListener('appinstalled', () => {
+  // Hide the app-provided install promotion
+  hideInstallPromotion();
+  // Clear the deferredPrompt so it can be garbage collected
+  deferredPrompt = null;
+  // Optionally, send analytics event to indicate successful install
+  console.log('PWA was installed');
+});
+buttonInstall.addEventListener('click', async () => {
+  // Hide the app provided install promotion
+  hideInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt and can't use it again, throw it away
+  deferredPrompt = null;
+});
 
 
 
